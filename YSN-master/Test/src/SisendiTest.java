ac7e9bc97d370cbd198f18acd2a79f7f7dadec57
Mihkel Kohava <mihkelkohava@gmail.com>
2016-03-12 16:06:10 +0200

    tere

---
+++ YSN-master/Test/src/SisendiTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ATM on 12/03/2016.
+ */
+import javax.swing.JOptionPane;
+public class SisendiTest {
+    public static void main(String[] args) {
+        String sisestatakse = JOptionPane.showInputDialog(null, "Sisesta midagi ", "Andmete sisestamine",
+                JOptionPane.QUESTION_MESSAGE);
+    }
+}
